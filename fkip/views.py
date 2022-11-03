789b585268be0f964f7db4f156294c868299efff
dafarihadatulaisy <106305682+dafarihadatulaisy@users.noreply.github.com>
2022-11-03 22:40:09 +0700

    Update views.py

--- fkip/views.py
+++ fkip/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Dosen, Mahasiswa, Staff
 from fkip.forms import FormDosen, FormStaff, FormMahasiswa
+from django.contrib import messages
 
 # Create your views here.
 def prodi6(request):
@@ -34,6 +35,7 @@ def tambah_staff (request):
         }
 
         return render(request, 'tambah-staff.html', konteks)
+
 
 def tambah_dosen (request):
     if request.POST:
@@ -77,3 +79,77 @@ def tambah_mahasiswa (request):
 
         return render(request, 'tambah-mahasiswa.html', konteks)
 
+def ubah_dosen(request, id_dosen):
+    dosen = Dosen.objects.get(id=id_dosen)
+    template = 'ubah-dosen.html'
+    if request.POST:
+        form = FormDosen (request.POST, instance=dosen)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Data Berhasil disimpan")
+            return redirect('ubah_dosen', id_dosen=id_dosen)
+
+    else:
+        form = FormDosen(instance=dosen)
+        konteks = {
+            'form':form,
+            'dosen':dosen,
+
+        }
+    return render(request, template, konteks)
+
+def ubah_mahasiswa(request, id_mahasiswa):
+    mahasiswa = Mahasiswa.objects.get(id=id_mahasiswa)
+    template = 'ubah-mahasiswa.html'
+    if request.POST:
+        form = FormMahasiswa (request.POST, instance=mahasiswa)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Data Berhasil disimpan")
+            return redirect('ubah_mahasiswa', id_mahasiswa=id_mahasiswa)
+
+    else:
+        form = FormMahasiswa(instance=mahasiswa)
+        konteks = {
+            'form':form,
+            'mahasiswa':mahasiswa,
+
+        }
+    return render(request, template, konteks)
+
+def ubah_staff(request, id_staff):
+    staff = Staff.objects.get(id=id_staff)
+    template = 'ubah-staff.html'
+    if request.POST:
+        form = FormStaff (request.POST, instance=staff)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Data Berhasil disimpan")
+            return redirect('ubah_staff', id_staff=id_staff)
+
+    else:
+        form = FormStaff(instance=staff)
+        konteks = {
+            'form':form,
+            'staff':staff,
+
+        }
+    return render(request, template, konteks)
+
+def hapus_dosen(request, id_dosen):
+    dosen = Dosen.objects.filter(id=id_dosen)
+    dosen.delete()
+
+    return redirect('fkip')
+
+def hapus_mahasiswa(request, id_mahasiswa):
+    mahasiswa = Mahasiswa.objects.filter(id=id_mahasiswa)
+    mahasiswa.delete()
+
+    return redirect('fkip')
+
+def hapus_staff(request, id_staff):
+    staff = Staff.objects.filter(id=id_staff)
+    staff.delete()
+
+    return redirect('fkip')
